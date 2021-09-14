2e6afb3f742b1be2211344c678333add3817e526
ProjetoWalletUp <projetowalletup@hotmail.com>
2021-09-14 01:27:44 +0000

    beta-Atualizado

--- walletup/src/components/layout/Menu.jsx
+++ walletup/src/components/layout/Menu.jsx
@@ -5,21 +5,22 @@ import { Link } from 'react-router-dom'
 const Menu = props => (
     <aside className='Menu'>
         <nav>
+            <div className='logo'>Logo</div>
             <ul>
                 <li>
                     <Link to="/">Home</Link>
                 </li>
                 <li>
-                    <Link to="/conteudo01/123">Conteúdo 01</Link>
+                    <Link to="/conteudo01">Carteira</Link>
                 </li>
                 <li>
-                    <Link to="/conteudo02">Conteúdo 02</Link>
+                    <Link to="/conteudo02">Sobre</Link>
                 </li>
                 <li>
-                    <Link to="/conteudo03">Conteúdo 03</Link>
+                    <Link to="/conteudo03">Contate-nos</Link>
                 </li>
                 <li>
-                    <Link to="/conteudo04">Conteúdo 04</Link>
+                    <Link to="/conteudo04">Login</Link>
                 </li>
             </ul>
         </nav>
--- walletup/src/views/contents/Home.jsx
+++ walletup/src/views/contents/Home.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Home = props => (
     <div>
-        <h1>Inicial</h1>
+        <h1>Bem Vindo</h1>
     </div>
 )
 
