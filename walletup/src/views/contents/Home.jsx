f234062508f7c9d719cf77f540ce07da0e5fe979
ProjetoWalletUp <projetowalletup@hotmail.com>
2021-09-14 14:26:53 +0000

    Beta Atualizado

--- walletup/src/views/contents/Home.jsx
+++ walletup/src/views/contents/Home.jsx
@@ -1,8 +1,22 @@
 import React from "react"
+import "./Home.css"
+import foto from "./assets/Box2.jpg"
 
 const Home = props => (
-    <div>
-        <h1>Bem Vindo</h1>
+    <div className="Container">
+        <div className="box1">
+            <title>Bem Vindo a WalletUp</title>
+             Simule e gerencie seus investimentos em fundos imobiliarios, descubra seus divindendos 
+        </div>
+        <div className="box2">
+            <img src={foto} alt="foto" height="200" />
+        </div>
+        <div className="box3">
+            A walletUp esta focada em te ajudar a ter acesso a investimentos em fundos imobiliarios, deixe conosco, apenas nos de os dados do seu 
+            investimento alvo, e nos iremos calcular e simular a projeção do ativo com o tempo.
+            O principal objetivo da WalletUp e tornar o mundo dos investimentos em fundo imobiliario acessivel a todos, e mostrar uma nova opção de renda passiva
+            alem de tornar o processo de aprender a ter uma vida financeira mais estavel bem mais facil.
+        </div>
     </div>
 )
 
