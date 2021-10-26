a5d50d35fc8d1728dcecac1d75d273eb798fd4b4
ProjetoWalletUp <projetowalletup@hotmail.com>
2021-10-26 18:31:22 +0000

    att

--- walletup/src/components/layout/Footer.jsx
+++ walletup/src/components/layout/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
           <MDBRow className="py-4 d-flex align-items-center">
             <MDBCol md="6" lg="5" className="text-center text-md-left mb-4 mb-md-0">
               <h6 className="mb-0 white-text">
-                Get connected with us on social networks!
+                Visite nossas redes sociais!
               </h6>
             </MDBCol>
             <MDBCol md="6" lg="7" className="text-center text-md-right">
